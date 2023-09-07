6011fa79bbe22f99c38f78a84db4b98f332aa548
agryz01 <agryz@yandex.ru>
2023-09-07 12:42:59 +0300

    updatUser

--- API_TG_Bot/controllers/users.js
+++ API_TG_Bot/controllers/users.js
@@ -3,26 +3,27 @@ const User = require('../models/user');
 
 const BadRequestErr = require('../errors/BadRequestErr');
 const ConflictErr = require('../errors/ConflictErr');
+const NotFoundErr = require('../errors/NotFoundErr');
 
 const createUser = (req, res, next) => {
   const { email, firstName } = req.body;
   User.create({ email, firstName })
     .then(user => res.send({ data: user }))
-    .catch ((err) => {
-  if (err instanceof mongoose.Error.ValidationError) {
-    next(new BadRequestErr('Ошибка валидации'));
-    return;
-  }
-  if (err.code === 11000) {
-    next(new ConflictErr('Пользователь с таким email уже существует'));
-    return;
-  }
-  res.status(500).send({ message: 'Произошла ошибка' });
-});
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        next(new BadRequestErr('Ошибка валидации'));
+        return;
+      }
+      if (err.code === 11000) {
+        next(new ConflictErr('Пользователь с таким email уже существует'));
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
 }
 
 const updateUser = (req, res, next) => {
-  User.findByIdAndUpdate(req.body._id, req.body, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.body._id, req.body, { new: true, runValidators: true }).orFail(new NotFoundErr('Запись с указанным _id не найдена'))
     .then((user) => res.send(user))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
