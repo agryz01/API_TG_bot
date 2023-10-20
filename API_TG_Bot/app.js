23952d0ee9dc6eb814e2eeba97436f876ff3f603
agryz01 <agryz@yandex.ru>
2023-10-20 13:52:34 +0300

    CORS

--- API_TG_Bot/app.js
+++ API_TG_Bot/app.js
@@ -8,7 +8,7 @@ const { handlerErrors } = require('./middlewares/handlerErrors');
 
 const { MONGODB_URL } = require('./utils/mongoConfig');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 
@@ -16,6 +16,27 @@ app.use(express.json());
 app.use(cookieParser());
 
 mongoose.connect(MONGODB_URL);
+
+const allowedCors = [
+    'http://localhost:3000'
+  ];
+  
+  app.use((req, res, next) => {
+    const { origin } = req.headers;
+    const { method } = req;
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+    const requestHeaders = req.headers['access-control-request-headers'];
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Credentials', true);
+    }
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      return res.end();
+    }
+    return next();
+  });
 
 app.use(routes);
 
